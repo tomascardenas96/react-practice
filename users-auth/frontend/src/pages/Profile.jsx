2a65ebd60209b54fd53110156024a39fd65e8fad
Tomás Cárdenas <tomicardenas96@gmail.com>
2024-01-16 10:02:15 -0300

    protected routes

--- users-auth/frontend/src/pages/Profile.jsx
+++ users-auth/frontend/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Spinner from "../components/Spinner";
 import "./Profile.css";
 
